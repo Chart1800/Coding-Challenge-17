e19705ba9eb19b5e13094c698eee00281cb95c34
Chart1800 <cameronh11@usf.edu>
2024-07-12 14:33:54 -0400

    tasks.js

--- tasks.js
+++ tasks.js
@@ -4,8 +4,8 @@ const tasks = [
   { id: 1, name: "Check Inventory Count", isCompleted: false },
   { id: 2, name: "Schedule Next Produce Shipment", isCompleted: false },
   { id: 3, name: "Make Staff Schedule For Next 2 Weeks", isCompleted: false },
-  { id: 4, name: "Count Register At Closing Time", isCompleted: false },
-  { id: 5, name: "Make Sure All Trash Is Taken", isCompleted: false },
+  { id: 4, name: "Count Register Count at 12:00pm", isCompleted: false },
+  { id: 5, name: "Make Sure All Trash Is Taken Out", isCompleted: false },
 
 ];
 
